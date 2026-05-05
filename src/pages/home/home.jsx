43be61dae612f16e4e458e5ef5007783201dbf97
Maria Dias <maria.l.silva360@aluno.senai.br>
2026-05-05 09:22:45 -0300

    Home - formatação css e adicional html

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,4 +1,4 @@
-import styles from './Home.module.css';
+import styles from './home.module.css';
 import Cards from '../../components/Cards/Cards'
 
 
@@ -8,14 +8,15 @@ import Cards from '../../components/Cards/Cards'
 
 function Home() {
   return (
-    <section className={styles.homePage}>
-
-      <h2 className={styles.title}></h2>
-
-      <h2>
-        aqui é um espaço de resistência ao consumo exagerado, <br />à produção descartável e às <br /> tendências que ignoram o povo.
-
-      </h2>
+<section className={styles.homePage}>
+      <div className={styles.bannerContainer}>
+<img src="/img/image.png" alt="Banner" className={styles.bannerImg} />     
+   <h2 className={styles.bannerTitle}>
+          Aqui é um espaço de resistência ao consumo exagerado, <br />
+          à produção descartável e às <br />
+          tendências que ignoram o povo.
+        </h2>
+      </div>
 
       <div className={styles.divbranca1}>
 
@@ -23,11 +24,14 @@ function Home() {
 
           Vestir-se é um ato político. <br />
           É escolher circular ao invés de descartar.
-          É apoiar uma moda mais justa, acessível e consciente
+          É apoiar uma moda mais justa e acessível.
         </p>
       </div>
 
-
+<p className={styles.frase}>      
+        Neste site, você pode cadastrar e gerenciar produtos de forma simples, 
+            promovendo a reutilização e o consumo consciente.
+          </p>
 
 
 
